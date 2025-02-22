1fe422cf98555a6c4537091b47ab05cca8baa316
al <alvicoto@gmail.com>
2025-02-22 10:17:24 -0600

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Prime Pizza</h1>
+      </header>
+  
+      <img src='images/pizza_photo.png' />
+      <p>Pizza is great.</p>
+  
+    </div>
+  );
+}
+
+export default App;
