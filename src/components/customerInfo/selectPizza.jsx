5a77d128170f21290e13d337147974f2e6137275
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-01 11:42:58 -0600

    Add pizza selection layout and API integration

---
+++ src/components/customerInfo/selectPizza.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SelectPizza() {
+    return (
+        <div>
+            <h2>Step 1: Select Your Pizza</h2>
+
+            {/* Pizza List Container */}
+            <div className="pizza-container">
+                {/* Pizza items will go here */}
+            </div>
+        </div>
+    );
+}
+
+export default SelectPizza;
