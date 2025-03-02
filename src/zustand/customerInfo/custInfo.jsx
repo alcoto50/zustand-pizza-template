7c542df792c192e1445505a66037e02eb94aa510
al cotton <alvicoto@gmail.com>
2025-03-02 17:43:31 -0600

    input forms

--- src/zustand/customerInfo/custInfo.jsx
+++ src/zustand/customerInfo/custInfo.jsx
@@ -4,12 +4,47 @@ import { useStore } from './store';
 import { Link } from 'react-router-dom';
 import wireframe from './wireframe.png';
 
+<div>
+    <h1>Customer Information</h1>
+    <h2>pizza</h2>
+</div>
+
 const custInfo = () => {
-    return (
-        <div>
-        <h1>Customer Information</h1>
-        <h2>pizza</h2>
+    const [customerInfo, setCustomerInfo] = useState({
+        customer_name: '',
+        street_address:'',
+        city: '',
+        zip: '',
+        type: 'pickup',
+    });
+    
+    const [total, setTotal] = useState(0);
+
+    const handleChange = (e) => {
+        setCustomerInfo({
+            ...customerInfo,
+            [e.target.name]: e.target.value
+        });
+
+        const handleSubmit = () => {
+            //navigate to checkout
+        };
+
+        return (
+            <div>
+                <h1>Enter Your Information </h1>
+                <input name='customer_name' onChange={handleChange} placeHolder='name'/>
+                <input name='street_address' onChange={handleChange} placeHolder='address'/>
+                <input name='city' onChange={handleChange} placeHolder='city'/>
+                <input name='zip' onChange={handleChange} placeHolder='zip'/>
+                <select name='type' onChange={handleChange}>
+                    <option value='pickup'>Pickup</option>
+                    <option value='delivery'>Delivery</option>
+            </select>
+            <div> Total: ${total.toFixed(2)}</div>
+        <button onClick={handleSubmit}>Checkout</button>
         </div>
-    );
+        );
+    }
 }
 export default custInfo;
